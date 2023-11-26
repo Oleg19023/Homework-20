c0d90dbf8d711624bf74ac3c4d7e4c63c0196a05
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2023-11-26 20:06:20 +0200

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,29 @@
-function calculateLoanPayments(loanAmount, annualInterestRate, loanTermMonths) {
-    const monthlyInterestRate = Math.round((annualInterestRate / 100 / 12) * 100) / 100;
+let loanTermMonths = 12;
+let loanAmount = 100;
+let annualInterestRate = 60;
 
-    let remainingLoanAmount = loanAmount;
-    let totalPayment = 0;
+let telo = Math.round((loanAmount / loanTermMonths) * 100) / 100;
+let interest = Math.round((loanAmount * (annualInterestRate / 100)) * 100) / 100;
+let totalPayments = 0;
 
-    const monthlyPayments = [];
+console.log('Начальная сумма кредита:', loanAmount);
+console.log('Тело кредита в месяц:', telo);
+console.log('Проценты в месяц:', interest);
 
-    const firstMonth = {
-        month: 1,
-        remainingLoanAmount: remainingLoanAmount,
-        principalPayment: Math.round((loanAmount / loanTermMonths) * 100) / 100,
-        interestPayment: Math.round(remainingLoanAmount * monthlyInterestRate * 100) / 100,
-        totalPayment: (
-            Math.round((loanAmount / loanTermMonths + remainingLoanAmount * monthlyInterestRate) * 100) / 100
-        ).toFixed(2),
-    };
+for (let i = 1; i <= loanTermMonths; i++) {
+    let remainingLoanAmount = loanAmount - (i === 1 ? 0 : telo * (i - 1));
+    let interestPayment = Math.round((remainingLoanAmount * (annualInterestRate / 100 / 12)) * 100) / 100;
+    let principalPayment = Math.round(telo * 100) / 100;
+    let totalPayment = Math.round((principalPayment + interestPayment) * 100) / 100;
 
-    monthlyPayments.push(firstMonth);
-    totalPayment += parseFloat(firstMonth.totalPayment);
-
-    for (let month = 2; month <= loanTermMonths; month++) {
-        const interestPayment = Math.round(remainingLoanAmount * monthlyInterestRate * 100) / 100;
-        let principalPayment = Math.round((loanAmount / loanTermMonths) * 100) / 100;
-        remainingLoanAmount -= principalPayment;
-        totalPayment += principalPayment + interestPayment;
-        totalPayment=Math.round((principalPayment + interestPayment) * 100) / 100
-        if(month==loanTermMonths){
-            principalPayment=remainingLoanAmount;
-            totalPayment=Math.round((principalPayment + interestPayment) * 100) / 100
-        }
-
-        monthlyPayments.push({
-            month,
-            remainingLoanAmount: Math.round(remainingLoanAmount * 100) / 100,
-            principalPayment: Math.round(principalPayment * 100) / 100,
-            interestPayment: Math.round(interestPayment * 100) / 100,
-            totalPayment: totalPayment
-        });
+    if (i === loanTermMonths) {
+        telo = Math.round(remainingLoanAmount * 100) / 100;
+        totalPayment = Math.round((telo + interest) * 100) / 100;
     }
 
-    totalPayment = Math.round(totalPayment * 100) / 100;
+    totalPayments += totalPayment;
 
-    const lastMonth = monthlyPayments[monthlyPayments.length - 1];
-    const diff = totalPayment - lastMonth.totalPayment;
-    return { monthlyPayments, totalPayment };
+    console.log(`Месяц ${i}: `, Math.round(remainingLoanAmount * 100) / 100, telo, interestPayment, totalPayment);
 }
 
-const loanAmount = 100; // Сумма кредита
-const annualInterestRate = 60; // Годовая процентная ставка
-const loanTermMonths = 12; // Срок кредитования в месяцах
-
-const { monthlyPayments, totalPayment } = calculateLoanPayments(loanAmount, annualInterestRate, loanTermMonths);
-
-console.log(monthlyPayments);
-console.log('Общая переплата:', Math.round(monthlyPayments.reduce((a, cv) => a + cv.interestPayment, 0)*100)/100 );
+console.log('Общая переплата:', Math.round(totalPayments * 100) / 100);
