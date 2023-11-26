41fe7b2fc0dec48e530da39dc00cbd52e8bf8946
DenisKvak1 <denis.kvak457@gmail.com>
2023-11-26 21:38:59 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ let totalPayments = 0;
 console.log('Начальная сумма кредита:', loanAmount);
 console.log('Тело кредита в месяц:', telo);
 console.log('Проценты в месяц:', interest);
-
+let cum=0;
 for (let i = 1; i <= loanTermMonths; i++) {
     let remainingLoanAmount = loanAmount - (i === 1 ? 0 : telo * (i - 1));
     let interestPayment = Math.round((remainingLoanAmount * (annualInterestRate / 100 / 12)) * 100) / 100;
@@ -21,9 +21,9 @@ for (let i = 1; i <= loanTermMonths; i++) {
         totalPayment = Math.round((telo + interestPayment) * 100) / 100;
     }
 
-    totalPayments += totalPayment;
+    cum=cum+interestPayment
 
     console.log(`Месяц ${i}: `, Math.round(remainingLoanAmount * 100) / 100, telo, interestPayment, totalPayment);
 }
 
-console.log('Общая переплата:', Math.round(totalPayments * 100) / 100);
+console.log('Общая переплата:', Math.round(cum * 100) / 100);
